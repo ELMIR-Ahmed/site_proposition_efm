34c286b96de7031b96579a37aacb1656047a1ae0
ELMIR-Ahmed <ahmedel@gmail.com>
2024-03-16 23:59:51 +0000

    adding routes & controller for Filiere model

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ const db = require('./models'); // Importez votre fichier de modèles Sequelize
 const app = express();
 const personnelRoutes = require('./routes/PersonnelRoutes')
 const Personnel_has_ModuleRoutes = require('./routes/Personnel_has_ModuleRoutes')
+const FiliereRoutes = require('./routes/FiliereRoutes')
 
 
 app.use(express.json())
 
 app.use('/personnel', personnelRoutes);
 app.use('/assignation', Personnel_has_ModuleRoutes);
+app.use('/filiere', FiliereRoutes);
 
 const port = process.env.PORT || 7000; // Utilisation de la variable d'environnement PORT, sinon le port 7000 par défaut
 
@@ -18,9 +20,6 @@ db.sequelize.sync({ force: false }).then(() => {
 }).catch(err => {
   console.error('Erreur de synchronisation de la base de données :', err);
 });
-
-
-
 
 app.listen(port, () => {
   console.log(`Le serveur écoute sur le port ${port}`);
