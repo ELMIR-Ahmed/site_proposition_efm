d59e9b759804fdbee1c1bc53536fae0432f79ddf
ELMIR-Ahmed <ahmedel@gmail.com>
2024-03-28 15:11:20 +0000

    adding authentication and authorization features

--- server/app.js
+++ server/app.js
@@ -1,18 +1,25 @@
 const express = require('express');
 const db = require('./models'); // Importez votre fichier de modèles Sequelize
 const app = express();
+const checkAuthenticationANDRole = require('./middlewares/authMiddleware')
 const personnelRoutes = require('./routes/PersonnelRoutes')
 const Personnel_has_ModuleRoutes = require('./routes/Personnel_has_ModuleRoutes')
 const FiliereRoutes = require('./routes/FiliereRoutes')
 const ModuleRoutes = require('./routes/ModuleRoutes')
+const LoginRoute = require('./routes/loginRoute')
+
+require('dotenv').config()
 
 
 app.use(express.json())
 
-app.use('/personnel', personnelRoutes);
-app.use('/assignation', Personnel_has_ModuleRoutes);
-app.use('/filiere', FiliereRoutes);
-app.use('/module', ModuleRoutes);
+// app.use(authMiddleware)
+
+app.use('/personnel', checkAuthenticationANDRole('directeur'), personnelRoutes);
+app.use('/assignation', checkAuthenticationANDRole('directeur'), Personnel_has_ModuleRoutes);
+app.use('/filiere', checkAuthenticationANDRole('directeur'), FiliereRoutes);
+app.use('/module', checkAuthenticationANDRole('directeur'), ModuleRoutes);
+app.use('/login', LoginRoute)
 
 const port = process.env.PORT || 7000; // Utilisation de la variable d'environnement PORT, sinon le port 7000 par défaut
 
---
+++ server/controllers/LoginController.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+const { Personnel } = require('../models')
+const Login = async (req, res) => {
+  try {
+    const {CIN, motDePasse} = req.body
+    const personnel = await Personnel.findByPk(CIN)
+    if (!personnel) {
+      return res.status(400).json({ "message" : "CIN ou Mot de Passe Incorrect !" })
+    }
+    const comparedPasswords = await bcrypt.compare(motDePasse, personnel.motDePasse)
+
+    if (!comparedPasswords) {
+      return res.status(400).json({ "message" : "CIN ou Mot de Passe Incorrect !" })
+    }
+
+    const AccessToken = (cin, role) => {
+      return jwt.sign(
+        { personnelCIN : cin, fonction : role }, /* Payload */
+        process.env.ACCESS_TOKEN_SECRET, /* clé secret */
+        { expiresIn: '24h' }, /* Date d'expiration du token */
+      )
+    }
+    const token = AccessToken(CIN, personnel.fonction)
+    res.status(200).send({"token" : token, "fonction" : personnel.fonction})
+
+  } catch (error) {
+    res.status(500).json({ "message" : "Une erreur est survenue lors de l'authentification !" + error })
+  }
+}
+
+module.exports = Login
